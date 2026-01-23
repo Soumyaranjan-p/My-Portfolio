72e21bc5818a15d0e31f514d46fbc7bc26ad8a90
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2026-01-23 14:03:19 +0530

    fixed footer responsive

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -5,10 +5,13 @@ import { footerConfig } from '@/app/config/Footer';
 export default function Footer() {
   return (
     <Container className="py-16">
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-sm text-secondary text-center">
+      {/* <div className="flex flex-col items-center justify-center"> */}
+      <div className="flex items-center justify-center md:items-start md:justify-start">
+        <p className="text-[12px] md:text-[15px] lg:text-[20px] text-[#8E8F8F] dark:text-[#8E8F8F] font-semibold">
+           &copy;{' '}{ new Date().getFullYear()} <b>{footerConfig.developer}</b> 
+        {/* <p className="text-sm text-secondary text-center">
           {footerConfig.text} <b>{footerConfig.developer}</b> <br /> &copy;{' '}
-          {new Date().getFullYear()}. {footerConfig.copyright}
+          {new Date().getFullYear()}. {footerConfig.copyright} */}
         </p>
       </div>
     </Container>
--- app/config/Footer.tsx
+++ app/config/Footer.tsx
@@ -1,5 +1,7 @@
 export const footerConfig = {
-  developer: 'Soumya Ranjan',
-  text: ' Developed by',
+  // developer: 'Soumya Ranjan',
+  // text: ' Developed by',
+   developer: 'Soumya Ranjan',
+  text: '',
   copyright: ' All rights reserved.',
 };
